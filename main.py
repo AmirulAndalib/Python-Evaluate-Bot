4bcb922fef1ecf45dba8690f3488179fc43ad9d3
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-13 22:00:29 +0530

    Added start command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from pyrogram import Client
+from pyrogram import Client, filters
 
 FayasNoushad = Client(
     "Python-Evaluate-Bot",
@@ -7,5 +7,25 @@ FayasNoushad = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+START_TEXT = """
+Hello {}, I am a link shortner telegram bot.
+
+Made by @FayasNoushad
+"""
+
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
+        ]]
+    )
+
+@FayasNoushad.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        reply_markup=START_BUTTONS,
+        disable_web_page_preview=True
+    )
 
 FayasNoushad.run()
