1da67c4f539d27c3abee00d4183d91b545a06e94
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-19 20:51:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def start(bot, update):
 
 @FayasNoushad.on_message(filters.private & filters.reply & filters.command(["eval", "evaluate", "run"]))
 async def eval(bot, update):
-    code = update.message.reply_to_message.text
+    code = update.reply_to_message.text
     try:
         output = float(eval(code))
         if len(output) < 4096:
